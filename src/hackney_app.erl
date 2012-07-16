3fe3559bf1be6c91001e59f3966c7a57daf43489
benoitc <bchesneau@gmail.com>
2012-07-16 09:43:50 +0200

    add application dependencies

--- src/hackney_app.erl
+++ src/hackney_app.erl
@@ -17,7 +17,21 @@
 %% ===================================================================
 
 start(_StartType, _StartArgs) ->
+    {ok, Deps} = application:get_key(hackney, applications),
+    true = lists:all(fun ensure_started/1, Deps),
     hackney_sup:start_link().
 
 stop(_State) ->
     ok.
+
+
+ensure_started(App) ->
+    case application:start(App) of
+        ok ->
+            true;
+        {error, {already_started, App}} ->
+            true;
+        Else ->
+            error_logger:error_msg("Couldn't start ~p: ~p", [App, Else]),
+            Else
+    end.
