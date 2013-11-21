0e4110b04e16878b356a78977dfc2c54547242cc
benoitc <bchesneau@gmail.com>
2013-11-21 14:36:35 +0100

    fix example

--- src/hackney_form.erl
+++ src/hackney_form.erl
@@ -15,27 +15,9 @@
 
 %% @doc encode a list of properties in a form.
 encode_form(KVs) ->
-    encode_form(KVs, []).
-
-
-%% @private
-encode_form([], Acc) ->
-    Lines = hackney_util:join(lists:reverse(Acc), <<"&">>),
+    Lines = hackney_url:qs(KVs),
     CType = <<"application/x-www-form-urlencoded; charset=utf-8">>,
-    {erlang:byte_size(Lines), CType, Lines};
-encode_form([{K,V}|R], Acc) ->
-    K1 = hackney_url:urlencode(K),
-    V1 = hackney_url:urlencode(V),
-    Line = << K1/binary, "=", V1/binary >>,
-    encode_form(R, [Line | Acc]).
+    {erlang:byte_size(Lines), CType, Lines}.
 
-decode_form(<<>>) ->
-    [];
 decode_form(Bin) ->
-    Tokens = binary:split(Bin, <<"&">>, [trim, global]),
-    [case binary:split(Token, <<"=">>, [trim]) of
-            [T] ->
-                {hackney_url:urldecode(T), true};
-            [Name, Value] ->
-                {hackney_url:urldecode(Name), hackney_url:urldecode(Value)}
-        end || Token <- Tokens].
+    hackney_url:parse_qs(Bin).
