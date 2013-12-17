688f82846998429ca69f61bf94f99d1912a51c9b
benoitc <bchesneau@gmail.com>
2013-12-17 12:27:30 +0100

    allows creation of cookies dynamically.
    A user can now pass {Name, Value, Opts} to the cookie option so he
    doesn't have to create the cookie using `hackney_cookie:setcookie/3` .

--- src/hackney_request.erl
+++ src/hackney_request.erl
@@ -459,6 +459,17 @@ maybe_add_cookies([], Headers) ->
     Headers;
 maybe_add_cookies(Cookie, Headers) when is_binary(Cookie) ->
     Headers ++ [{<<"Cookie">>, Cookie}];
+maybe_add_cookies({Name, Value, Opts}, Headers) ->
+    Cookie = hackney_cookie:setcookie(Name, Value, Opts),
+    Headers ++ [{<<"Cookie">>, Cookie}];
+maybe_add_cookies([{Name, Value} | Rest], Headers) ->
+    Cookie = hackney_cookie:setcookie(Name, Value, []),
+    Headers1 = Headers ++ [{<<"Cookie">>, Cookie}],
+    maybe_add_cookies(Rest, Headers1);
+maybe_add_cookies([{Name, Value, Opts} | Rest], Headers) ->
+    Cookie = hackney_cookie:setcookie(Name, Value, Opts),
+    Headers1 = Headers ++ [{<<"Cookie">>, Cookie}],
+    maybe_add_cookies(Rest, Headers1);
 maybe_add_cookies([Cookie | Rest], Headers) ->
     Headers1 = Headers ++ [{<<"Cookie">>, Cookie}],
     maybe_add_cookies(Rest, Headers1).
