64f0f8b675e9d2f59f9a12ef479010f19dcc5671
benoitc <bchesneau@gmail.com>
2013-12-30 13:36:58 +0100

    keep the default to 4096.

--- include/hackney.hrl
+++ include/hackney.hrl
@@ -26,7 +26,7 @@
         send_fun=nil,
         body_state=waiting,
         multipart=nil,
-        req_chunk_size=65536000, %% 64 MB is the default
+        req_chunk_size=4096,
         buffer = <<>>,
         partial_headers = [],
         version,
