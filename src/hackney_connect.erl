8ac5f300de3eca18aa4938a4d55658ddbe91fcb6
benoitc <bchesneau@gmail.com>
2014-02-22 11:45:43 +0100

    hackney_connect: Host should be given as a string
    fix #83

--- src/hackney_connect.erl
+++ src/hackney_connect.erl
@@ -24,6 +24,8 @@ connect(Transport, Host, Port) ->
 connect(Transport, Host, Port, Options) ->
     connect(Transport, Host, Port, Options, false).
 
+connect(Transport, Host, Port, Options, Dynamic) when is_binary(Host) ->
+    connect(Transport, binary_to_list(Host), Port, Options, Dynamic);
 connect(Transport, Host, Port, Options, Dynamic) ->
     case create_connection(Transport, Host, Port, Options, Dynamic) of
         {ok, #client{request_ref=Ref}} ->
