548a4e317de7c7f43963e8e873e6f8297a7604a1
Eduardo Gurgel <eduardo@gurgel.me>
2014-09-07 20:24:49 +1200

    Add integrated test for cookies

--- test/hackney_integration_tests.erl
+++ test/hackney_integration_tests.erl
@@ -9,7 +9,9 @@ http_requests_test_() ->
    fun(SetupData) ->
      [get_request(SetupData),
       basic_auth_request_failed(SetupData),
-      basic_auth_request(SetupData)]
+      basic_auth_request(SetupData),
+      set_cookie_request(SetupData),
+      send_cookies_request(SetupData)]
    end}.
 
 start() -> hackney:start().
@@ -32,3 +34,18 @@ basic_auth_request_failed(_) ->
   Options = [{basic_auth, {<<"wrong">>, <<"auth">>}}],
   {ok, StatusCode, _, _} = hackney:request(get, URL, [], <<>>, Options),
   ?_assertEqual(StatusCode, 401).
+
+set_cookie_request(_) ->
+  URL = <<"http://localhost:8000/cookies/set?k1=v1">>,
+  {ok, _, Headers, _} = hackney:request(get, URL, [], <<>>, []),
+  Cookies = hackney:cookies(Headers),
+  ExpectedCookies = [{<<"k1">>, [{<<"k1">>,<<"v1">>},{<<"Path">>,<<"/">>}]}],
+  ?_assertEqual(Cookies, ExpectedCookies).
+
+send_cookies_request(_) ->
+  URL = <<"http://localhost:8000/cookies">>,
+  Options = [{cookie, [{<<"SESSION">>, <<"123">>}]}],
+  {ok, _, _, Client} = hackney:request(get, URL, [], <<>>, Options),
+  {ok, Body} = hackney:body(Client),
+  Match = re:run(Body, <<".*\"SESSION\".*\"123\".*">>),
+  ?_assertMatch({match, _}, Match).
