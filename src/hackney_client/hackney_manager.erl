benoitc/hackney

commit 331c025cf83a81027032f880ca02fd828d975eeb

improve hackney performance
In the current versio ETS is a bottleneck since all states need to be
temporarely stored there. Instead of storing then in ETS we are now storing to
the local process dictionnary. Since the request is also local to the process
it will also improve GC.

src/hackney_client/hackney_manager.erl
@@ -16,6 +16,7 @@
 
 -export([get_state/1, get_state/2,
          update_state/1, update_state/2,
+         store_state/1, store_state/2,
          take_control/2,
          handle_error/1]).
 
@@ -41,9 +42,8 @@
 new_request(InitialState) ->
     %% initialize the request
     Ref = make_ref(),
-    Req = #request{ref=Ref,
-                   state=InitialState#client{request_ref=Ref}},
-    ets:insert(?MODULE, {Ref, Req}),
+    %% store the current state in the process dictionnary
+    put(Ref, InitialState#client{request_ref=Ref}),
     %% supervise the process owner
     ok = gen_server:call(?MODULE, {new_request, self(), Ref}),
     Ref.
@@ -53,10 +53,10 @@ new_request(InitialState) ->
 cancel_request(#client{request_ref=Ref}) ->
     cancel_request(Ref);
 cancel_request(Ref) when is_reference(Ref) ->
-    case ets:lookup(?MODULE, Ref) of
-        [] ->
+    case get_state(Ref) of
+        req_not_found ->
             req_not_found;
-        [{Ref, #request{state=Client}}] ->
+        Client ->
             #client{transport=Transport, socket=Socket,
                     buffer=Buffer, response_state=RespState} = Client,
 
@@ -64,7 +64,7 @@ cancel_request(Ref) when is_reference(Ref) ->
             case Transport:controlling_process(Socket, self()) of
                 ok ->
                     %% remove the request
-                    ets:delete(?MODULE, Ref),
+                    erase(Ref),
 
                     %% stop to monitor the request
                     case gen_server:call(?MODULE, {cancel_request, Ref}) of
@@ -81,16 +81,17 @@ cancel_request(Ref) when is_reference(Ref) ->
 
 close_request(#client{request_ref=Ref}) ->
     close_request(Ref);
+
 close_request(Ref) ->
-    case ets:lookup(?MODULE, Ref) of
-        [] ->
+    case get_state(Ref) of
+        req_not_found ->
             req_not_found;
-        [{Ref, #request{state=Client}}] ->
+        Client ->
             #client{transport=Transport, socket=Socket,
                     state=Status} = Client,
 
             %% remove the request
-            ets:delete(?MODULE, Ref),
+            erase(Ref),
 
             %% stop to monitor the request
             Reply = gen_server:call(?MODULE, {cancel_request, Ref}),
@@ -107,30 +108,41 @@ close_request(Ref) ->
     end.
 
 controlling_process(Ref, Pid) ->
-    case ets:lookup(?MODULE, Ref) of
-        [] ->
-            req_not_found;
-        [{Ref, #request{state=Client}}] ->
+    case get(Ref) of
+        undefined ->
+            {error, not_owner};
+        Client ->
             Reply = gen_server:call(?MODULE, {controlling_process, Ref, Pid}),
             case Reply of
                 ok ->
                     #client{transport=Transport, socket=Socket} = Client,
-                    Transport:controlling_process(Socket, Pid);
+                    Transport:controlling_process(Socket, Pid),
+                    ets:insert(?MODULE, #request{ref=Ref, state=Client}),
+                    ok;
                 Error ->
                     Error
             end
     end.
 
 start_async_response(Ref) ->
-    case ets:lookup(?MODULE, Ref) of
-        [] ->
+    case get_state(Ref)of
+        req_not_found ->
             req_not_found;
-        [{Ref, #request{state=Client}}] ->
+        Client ->
             #client{transport=Transport, socket=Socket,
                     stream_to=StreamTo} = Client,
             case gen_server:call(?MODULE, {start_async_response, Ref,
                                            StreamTo, Client}) of
                 {ok, Pid} ->
+                    %% store temporarely the socket in the the ets so it can
+                    %% be used by the other process later
+                    true = ets:insert(?MODULE, #request{ref=Ref,
+                                                        state=Client}),
+                    %% delete the current state from the process dictionnary
+                    %% since it's not the owner
+                    erase(Ref),
+
+                    %% transfert the control of the socket
                     Transport:controlling_process(Socket, Pid);
                 Error ->
                     Error
@@ -138,12 +150,7 @@ start_async_response(Ref) ->
     end.
 
 stop_async_response(Ref) ->
-    case ets:lookup(?MODULE, Ref) of
-        [] ->
-            {error, req_not_found};
-        [{Ref, _Req}] ->
-            gen_server:call(?MODULE, {stop_async_response, Ref, self()})
-    end.
+    gen_server:call(?MODULE, {stop_async_response, Ref, self()}, infinity).
 
 async_response_pid(Ref) ->
     case ets:lookup(?REFS, Ref) of
@@ -167,10 +174,20 @@ get_state(#client{request_ref=Ref}) ->
     get_state(Ref);
 
 get_state(Ref) ->
-    case ets:lookup(?MODULE, Ref) of
-        [] ->
-            req_not_found;
-        [{Ref, #request{state=State}}] ->
+    case get(Ref) of
+        undefined ->
+            case ets:lookup(?MODULE, Ref) of
+                [] ->
+                    req_not_found;
+                [{Ref, #request{state=State}}] ->
+                    %% store the state in the new context, only the current
+                    %% owner can handle it.
+                    put(Ref, State),
+                    %% delete the state, from ets
+                    ets:delete(?MODULE, Ref),
+                    State
+            end;
+        State ->
             State
     end.
 
@@ -184,19 +201,22 @@ update_state(#client{request_ref=Ref}=NState) ->
     update_state(Ref, NState).
 
 update_state(Ref, NState) ->
-    case ets:lookup(?MODULE, Ref) of
-        [] -> ok;
-        [{Ref, Req}] ->
-            true = ets:insert(?MODULE, {Ref, Req#request{state=NState}}),
-            ok
-    end.
+    put(Ref, NState).
+
+store_state(#client{request_ref=Ref}=NState) ->
+    store_state(Ref, NState).
+
+store_state(Ref, NState) ->
+    true = ets:insert(?MODULE, #request{ref=Ref, state=NState}),
+    ok.
+
 
 take_control(Ref, NState) ->
     case ets:lookup(?MODULE, Ref) of
         [] -> {error, req_not_found};
-        [{Ref, Req}] ->
-            true = ets:insert(?MODULE, {Ref, Req#request{pid=self(),
-                                                         state=NState}}),
+        [{Ref, _Req}] ->
+            ets:delete(?MODULE, Ref),
+            put(Ref, NState),
             ok
     end.
 

src/hackney_client/hackney_stream.erl
@@ -41,7 +41,9 @@ stream_loop(Parent, Owner, Ref, #client{transport=Transport,
                                          method= <<"HEAD">>,
                                          parser=Parser}=Client) ->
     Buffer = hackney_http:get(Parser, buffer),
-    hackney_manager:update_state(finish_response(Buffer, Client)),
+
+
+    hackney_manager:store_state(finish_response(Buffer, Client)),
     %% pass the control of the socket to the manager so we make
     %% sure a new request will be able to use it
     Transport:controlling_process(Socket, Parent),
@@ -54,7 +56,7 @@ stream_loop(Parent, Owner, Ref, #client{transport=Transport,
                                          parser=Parser}=Client)
         when TE /= <<"chunked">> ->
     Buffer = hackney_http:get(Parser, buffer),
-    hackney_manager:update_state(finish_response(Buffer, Client)),
+    hackney_manager:store_state(finish_response(Buffer, Client)),
     %% pass the control of the socket to the manager so we make
     %% sure a new request will be able to use it
     Transport:controlling_process(Socket, Parent),
@@ -96,12 +98,11 @@ maybe_continue(Parent, Owner, Ref, #client{transport=Transport,
             erlang:hibernate(?MODULE, maybe_continue, [Parent, Owner, Ref,
                                                        Client]);
         {Ref, stop_async, From} ->
-            hackney_manager:update_state(Client#client{async=false}),
+            hackney_manager:store_state(Client#client{async=false}),
             Transport:setopts(Socket, [{active, false}]),
             Transport:controlling_process(Socket, From),
             From ! {Ref, ok};
         {Ref, close} ->
-            hackney_manager:update_state(Client),
             hackney_response:close(Client);
         {system, From, Request} ->
             sys:handle_system_msg(Request, From, Parent, ?MODULE, [],
@@ -119,12 +120,11 @@ maybe_continue(Parent, Owner, Ref, #client{transport=Transport,
         {Ref, stream_next} ->
             stream_loop(Parent, Owner, Ref, Client);
         {Ref, stop_async, From} ->
-            hackney_manager:update_state(Client),
+            hackney_manager:store_state(Client),
             Transport:setopts(Socket, [{active, false}]),
             Transport:controlling_process(Socket, From),
             From ! {Ref, ok};
         {Ref, close} ->
-            hackney_manager:update_state(Client),
             hackney_response:close(Client);
         {system, From, Request} ->
             sys:handle_system_msg(Request, From, Parent, ?MODULE, [],
@@ -169,7 +169,7 @@ maybe_redirect(Parent, Owner, Ref, StatusInt, Reason,
                         {_, _} ->
                             case hackney_response:skip_body(Client2) of
                                 {ok, Client3} ->
-                                    hackney_manager:update_state(Client3),
+                                    hackney_manager:store_state(Client3),
                                     Owner ! {hackney_response, Ref,
                                              {redirect, Location, Headers}};
                                 Error ->
@@ -196,7 +196,7 @@ maybe_redirect(Parent, Owner, Ref, StatusInt, Reason,
                         Location ->
                             case hackney_response:skip_body(Client2) of
                                 {ok, Client3} ->
-                                    hackney_manager:update_state(Client3),
+                                    hackney_manager:store_state(Client3),
                                     Owner ! {hackney_response, Ref,
                                              {see_other, Location, Headers}};
                                 Error ->
@@ -240,7 +240,7 @@ async_recv(Parent, Owner, Ref,
         {Ref, close} ->
             Transport:close(Sock);
         {Ref, stop_async, From} ->
-            hackney_manager:update_state(Client#client{async=false}),
+            hackney_manager:store_state(Client#client{async=false}),
             Transport:setopts(Sock, [{active, false}]),
             Transport:controlling_process(Sock, From),
             From ! {Ref, ok};
@@ -331,11 +331,11 @@ process({ok, Data, NParser}, Client) ->
     {ok, Data, NClient};
 process({done, Rest}, Client) ->
     Client2 = finish_response(Rest, Client),
-    hackney_manager:update_state(Client2),
+    hackney_manager:store_state(Client2),
     done;
 process(done, Client) ->
     Client2 = finish_response(<<>>, Client),
-    hackney_manager:update_state(Client2),
+    hackney_manager:store_state(Client2),
     done;
 process({error, Reason}, _Client) ->
     {error, Reason};
@@ -343,9 +343,7 @@ process(Error, _Client) ->
     {error, Error}.
 
 update_client(Parser, Client) ->
-    NClient = Client#client{parser=Parser},
-    hackney_manager:update_state(NClient),
-    NClient.
+    Client#client{parser=Parser}.
 
 finish_response(Rest, Client0) ->
     Client = Client0#client{response_state=done,

src/hackney_connect/hackney_ssl_transport.erl
@@ -31,8 +31,9 @@ connect(Host, Port, Opts, Timeout) when is_list(Host), is_integer(Port),
                      linger, next_protocols_advertised, nodelay, password, raw,
                      reuse_session, reuse_sessions, secure_renegotiate,
                      send_timeout, send_timeout_close, verify,
-                     verify_fun, inet6, versions, server_name_indication, depth],
-    BaseOpts = [binary, {active, false}, {packet, raw}, {nodelay, true}],
+                     verify_fun, inet6, versions, server_name_indication,
+                     depth, reuseaddr],
+    BaseOpts = [binary, {active, false}, {packet, raw}, {reuseaddr, true}],
     Opts1 = hackney_util:filter_options(Opts, AcceptedOpts, BaseOpts),
 
     %% connect

src/hackney_connect/hackney_tcp_transport.erl
@@ -27,8 +27,8 @@ connect(Host, Port, Opts, Timeout) when is_list(Host), is_integer(Port),
 
     %% filter options
     AcceptedOpts =  [linger, nodelay, keepalive, send_timeout,
-                     send_timeout_close, raw, inet6],
-    BaseOpts = [binary, {active, false}, {packet, raw}, {nodelay, true}],
+                     send_timeout_close, raw, inet6, reuseaddr],
+    BaseOpts = [binary, {active, false}, {packet, raw}, {reuseaddr, true}],
     Opts1 = hackney_util:filter_options(Opts, AcceptedOpts, BaseOpts),
 
     %% connect

commit 3cc27a5580ea4690fe1e70b0e3af0d61f7acb64c

fix memory leak
fix memory leak issue introduced in 331c025cf83a81027032f880ca02fd828d975eeb

src/hackney_client/hackney_manager.erl
@@ -85,6 +85,7 @@ close_request(#client{}=Client) ->
 
     %% remove the request
     erase(Ref),
+    ets:delete(?MODULE, Ref),
 
     %% stop to monitor the request
     Reply = gen_server:call(?MODULE, {cancel_request, Ref}),