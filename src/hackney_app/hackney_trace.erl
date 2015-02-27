bc246f19ae10591129c2f541c09748fb5744b19c
Benoit Chesneau <bchesneau@gmail.com>
2015-02-28 00:12:44 +0100

    add license header

--- src/hackney_app/hackney_trace.erl
+++ src/hackney_app/hackney_trace.erl
@@ -1,3 +1,6 @@
+%%%% -*- erlang -*-
+%%%% This file is part of hackney released under the Apache 2 license.
+%%%% See the NOTICE for more information.
 -module(hackney_trace).
 
 -export([enable/2, disable/0,
