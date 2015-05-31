1dc3fa79c8ddf0f4b154e8576fe254fd1e99486d
Teemu Harju <teemu.harju@gmail.com>
2015-05-31 19:47:35 +0300

    Fixed status line parsing when reason phrase is missing entirely.

--- test/hackney_http_tests.erl
+++ test/hackney_http_tests.erl
@@ -15,3 +15,10 @@ parse_response_incomplete_200_test() ->
 	{response, _Version, StatusInt, Reason, _NState} =  hackney_http:parse_response_version(Response, St),
 	?assertEqual(StatusInt, 200),
 	?assertEqual(Reason, <<"">>).
+
+parse_response_missing_reason_phrase_test() ->
+    	Response = <<"HTTP/1.1 200">>,
+	St = #hparser{},
+	{response, _Version, StatusInt, Reason, _NState} =  hackney_http:parse_response_version(Response, St),
+	?assertEqual(StatusInt, 200),
+	?assertEqual(Reason, <<"">>).
