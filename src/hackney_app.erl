a90e28dd0bc45c876ff02d85556afe8e4126d7a4
Benoit Chesneau <bchesneau@gmail.com>
2018-04-02 19:44:12 +0200

    fix some notice lines

--- src/hackney_app.erl
+++ src/hackney_app.erl
@@ -3,8 +3,6 @@
 %%% This file is part of hackney released under the Apache 2 license.
 %%% See the NOTICE for more information.
 %%%
-%%% Copyright (c) 2012-2014 Benoît Chesneau <benoitc@e-engura.org>
-%%%
 
 -module(hackney_app).
 
