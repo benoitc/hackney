f00f54f3acc02bc35c0bce31e8b88238704f30ee
Jose M Perez <jose.m.perez.ramos+git@gmail.com>
2020-02-29 11:40:26 +0100

    Stream test: Rename for clarity and configurable receive timeout

--- test/hackney_stream_tests.erl
+++ test/hackney_stream_tests.erl
@@ -8,7 +8,7 @@
 all_tests() ->
    [
     async_request(),
-    heed_connection_close()
+    handle_connection_close()
    ].
 
 stream_test_() ->
@@ -39,13 +39,13 @@ async_request() ->
      ?_assertEqual([body, headers, status], Keys)].
 
 receive_response(Ref) ->
-    Dict = receive_response(Ref, orddict:new()),
+    Dict = receive_response(Ref, orddict:new(), infinity),
     Keys = orddict:fetch_keys(Dict),
     StatusCode = orddict:fetch(status, Dict),
     {StatusCode, Keys}.
 
 checkout(_Host, _Port, _Transport, _Client) -> {error, no_socket, make_ref()}.
-heed_connection_close() ->
+handle_connection_close() ->
     URL = <<"http://localhost:8000/get">>,
     Options = [async, pool],
 
@@ -57,24 +57,24 @@ heed_connection_close() ->
     application:set_env(hackney, pool_handler, hackney_pool),
 
     Socket = proplists:get_value(socket, hackney:request_info(ClientRef)),
-    Dict = receive_response(ClientRef, orddict:new()),
+    Dict = receive_response(ClientRef, orddict:new(), 5000),
     Headers = hackney_headers_new:from_list(orddict:fetch(headers, Dict)),
     CloseHeader = hackney_headers_new:get_value(<<"connection">>, Headers),
     [?_assertEqual(<<"close">>, CloseHeader),
      ?_assertEqual({error, closed}, gen_tcp:send(Socket, <<>>))
     ].
 
-receive_response(Ref, Dict0) ->
+receive_response(Ref, Dict0, Timeout) ->
     receive
         {hackney_response, Ref, {status, Status, _Reason}} ->
             Dict1 = orddict:store(status, Status, Dict0),
-            receive_response(Ref, Dict1);
+            receive_response(Ref, Dict1, Timeout);
         {hackney_response, Ref, {headers, Headers}} ->
             Dict1 = orddict:store(headers, Headers, Dict0),
-            receive_response(Ref, Dict1);
+            receive_response(Ref, Dict1, Timeout);
         {hackney_response, Ref, done} -> Dict0;
         {hackney_response, Ref, Bin} ->
             Dict1 = orddict:append(body, Bin, Dict0),
-            receive_response(Ref, Dict1)
-    after 5000 -> {error, timeout}
+            receive_response(Ref, Dict1, Timeout)
+    after Timeout -> {error, timeout}
     end.
