78ce833a88715b195a045c6c949dca6ea1dc7d7a
Jose M Perez <jose.m.perez.ramos+git@gmail.com>
2020-02-29 10:24:25 +0100

    Handle Connection: close response header for streams

--- test/hackney_stream_tests.erl
+++ test/hackney_stream_tests.erl
@@ -2,10 +2,13 @@
 -include_lib("eunit/include/eunit.hrl").
 -include("hackney_lib.hrl").
 
+-export([checkout/4]).
+
 %% This seems necessary to list the tests including the generator
 all_tests() ->
    [
-    async_request()
+    async_request(),
+    heed_connection_close()
    ].
 
 stream_test_() ->
@@ -41,6 +44,26 @@ receive_response(Ref) ->
     StatusCode = orddict:fetch(status, Dict),
     {StatusCode, Keys}.
 
+checkout(_Host, _Port, _Transport, _Client) -> {error, no_socket, make_ref()}.
+heed_connection_close() ->
+    URL = <<"http://localhost:8000/get">>,
+    Options = [async, pool],
+
+    % Notice that ?MODULE has checkout/4 but not checkin, so if this test
+    % passes, it means that checkin was not called (which is intended for closed
+    % sockets)
+    application:set_env(hackney, pool_handler, ?MODULE),
+    {ok, ClientRef} = hackney:get(URL, [], <<>>, Options),
+    application:set_env(hackney, pool_handler, hackney_pool),
+
+    Socket = proplists:get_value(socket, hackney:request_info(ClientRef)),
+    Dict = receive_response(ClientRef, orddict:new()),
+    Headers = hackney_headers_new:from_list(orddict:fetch(headers, Dict)),
+    CloseHeader = hackney_headers_new:get_value(<<"connection">>, Headers),
+    [?_assertEqual(<<"close">>, CloseHeader),
+     ?_assertEqual({error, closed}, gen_tcp:send(Socket, <<>>))
+    ].
+
 receive_response(Ref, Dict0) ->
     receive
         {hackney_response, Ref, {status, Status, _Reason}} ->
@@ -53,4 +76,5 @@ receive_response(Ref, Dict0) ->
         {hackney_response, Ref, Bin} ->
             Dict1 = orddict:append(body, Bin, Dict0),
             receive_response(Ref, Dict1)
+    after 5000 -> {error, timeout}
     end.
