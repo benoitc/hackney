2d44c6337a0e3ab0659ba49c1aaa0a709fb41213
Kyle Boe <kyle@boe.codes>
2023-07-18 21:12:04 -0700

    Fix partial_pathencode to properly handle asterisk (*)
    Ref:
    - https://github.com/edgurgel/httpoison/issues/471
    - https://github.com/benoitc/hackney/issues/399
    - https://github.com/benoitc/hackney/issues/277
    - https://github.com/benoitc/hackney/issues/272

--- src/hackney_url.erl
+++ src/hackney_url.erl
@@ -461,7 +461,7 @@ partial_pathencode(<<C, Rest/binary>> = Bin, Acc) ->
   if	C >= $0, C =< $9 -> partial_pathencode(Rest, <<Acc/binary, C>>);
     C >= $A, C =< $Z -> partial_pathencode(Rest, <<Acc/binary, C>>);
     C >= $a, C =< $z -> partial_pathencode(Rest, <<Acc/binary, C>>);
-    C =:= $;; C =:= $=; C =:= $,; C =:= $:; C =:= $@; C =:= $(; C =:= $) ->
+    C =:= $;; C =:= $=; C =:= $,; C =:= $:; C =:= $*; C =:= $@; C =:= $(; C =:= $) ->
       partial_pathencode(Rest, <<Acc/binary, C>>);
     C =:= $.; C =:= $-; C =:= $+; C =:= $~; C =:= $_ ->
       partial_pathencode(Rest, <<Acc/binary, C>>);
