benoitc/hackney

commit c232dee171cc584f716f398721a6116ea5b7bd66

feature: prefer IPV6 for connections
This change implements the happy eyeballs strategy to bnring better support of IPv6 for hackney.

It follows the recommendations iof the RFC 8305. https://datatracker.ietf.org/doc/html/rfc8305

src/hackney_ssl.erl
@@ -25,16 +25,7 @@
 %% @doc Atoms used to identify messages in {active, once | true} mode.
 messages(_) -> {ssl, ssl_closed, ssl_error}.
 
-%% @doc The ssl:connect/4 (and related) doesn't work with textual representation
-%% of IP addresses. It accepts either a string with a DNS-resolvable name or a
-%% tuple with parts of an IP as numbers. This function attempts to parse given
-%% string and either returns such tuple, or the string if it's impossible to
-%% parse.
-parse_address(Host) when is_list(Host) ->
-  case inet:parse_address(Host) of
-    {ok, Address} -> Address;
-    {error, _} -> Host
-  end.
+
 
 
 check_hostname_opts(Host0) ->
@@ -132,16 +123,19 @@ find(_Fun, []) ->
 
 
 connect(Host, Port, Opts) ->
-  connect(Host, Port, Opts, infinity).
+  connect(Host, Port, Opts, 30000).
 
-connect(Host, Port, Opts, Timeout) when is_list(Host), is_integer(Port),
-                                        (Timeout =:= infinity orelse is_integer(Timeout)) ->
+connect(Host, Port, Opts0, Timeout) when is_list(Host), is_integer(Port),
+                                        (Timeout =:= 5000 orelse is_integer(Timeout)) ->
+  SSLOpts = proplists:get_value(ssl_options, Opts0),
   BaseOpts = [binary, {active, false}, {packet, raw}],
-  Opts1 = hackney_util:merge_opts(BaseOpts, Opts),
-  %% connect
-  ssl:connect(parse_address(Host), Port, Opts1, Timeout).
-
-
+  Opts1 = hackney_util:merge_opts(BaseOpts, proplists:delete(ssl_options, Opts0)),
+  case hackney_happy:connect(Host, Port, Opts1, Timeout) of
+    {ok, Sock} ->
+      ssl:connect(Sock, SSLOpts);
+    Error ->
+      Error
+  end.
 
 recv(Socket, Length) ->
   recv(Socket, Length, infinity).

NOTICE
@@ -21,3 +21,6 @@ Copyright (c) 2009, Erlang Training and Consulting Ltd.
 Copyright (C) 1998 - 2014, Daniel Stenberg, <daniel@haxx.se>, et al.
 
 *) hackney_trace (C) 2015 under the Erlang Public LicensE
+
+*) hackney_cidr is based on inet_cidr 1.2.1. vendored for customer purpose.
+Copyright (c) 2024, Enki Multimedia , MIT License

src/hackney.erl
@@ -311,17 +311,18 @@ request(Method, #hackney_url{}=URL0, Headers0, Body, Options0) ->
   URL = hackney_url:normalize(URL0, PathEncodeFun),
 
   ?report_trace("request", [{method, Method},
-    {url, URL},
-    {headers, Headers0},
-    {body, Body},
-    {options, Options0}]),
+                            {url, URL},
+                            {headers, Headers0},
+                            {body, Body},
+                            {options, Options0}]),
 
   #hackney_url{transport=Transport,
-    host = Host,
-    port = Port,
-    user = User,
-    password = Password,
-    scheme = Scheme} = URL,
+               host = Host,
+               port = Port,
+               user = User,
+               password = Password,
+               scheme = Scheme} = URL,
+
 
   Options = case User of
               <<>> ->

src/hackney_connection.erl
@@ -102,27 +102,13 @@ connect_options(hackney_local_tcp, _Host, ClientOptions) ->
   proplists:get_value(connect_options, ClientOptions, []);
 
 connect_options(Transport, Host, ClientOptions) ->
-  ConnectOpts0 = proplists:get_value(connect_options, ClientOptions, []),
-
-  %% handle ipv6
-  ConnectOpts1 = case lists:member(inet, ConnectOpts0) orelse
-                      lists:member(inet6, ConnectOpts0) of
-                   true ->
-                     ConnectOpts0;
-                   false ->
-                     case hackney_util:is_ipv6(Host) of
-                       true ->
-                         [inet6 | ConnectOpts0];
-                       false ->
-                         ConnectOpts0
-                     end
-                 end,
+  ConnectOpts = proplists:get_value(connect_options, ClientOptions, []),
 
   case Transport of
     hackney_ssl ->
-      ConnectOpts1 ++ ssl_opts(Host, ClientOptions);
+      [{ssl_options, ssl_opts(Host, ClientOptions)} | ConnectOpts];
     _ ->
-      ConnectOpts1
+      ConnectOpts
   end.
 
 

src/hackney_happy.erl
@@ -1,0 +1,140 @@
+-module(hackney_happy).
+
+-export([connect/3, connect/4]).
+
+-include("hackney_internal.hrl").
+-include_lib("kernel/include/inet.hrl").
+
+-define(TIMEOUT, 250).
+-define(CONNECT_TIMEOUT, 5000).
+
+connect(Hostname, Port, Opts) ->
+  connect(Hostname, Port, Opts, ?CONNECT_TIMEOUT).
+
+connect(Hostname, Port, Opts, Timeout) ->
+  do_connect(parse_address(Hostname), Port, Opts, Timeout).
+
+do_connect(Hostname, Port, Opts, Timeout) when is_tuple(Hostname) ->
+  case hackney_cidr:is_ipv6(Hostname) of
+    true ->
+      ?report_debug("connect using IPv6", [{hostname, Hostname}, {port, Port}]),
+      gen_tcp:connect(Hostname, Port, [inet6 | Opts], Timeout);
+    false ->
+      case hackney_cidr:is_ipv4(Hostname) of
+        true ->
+          ?report_debug("connect using IPv4", [{hostname, Hostname}, {port, Port}]),
+          gen_tcp:connect(Hostname, Port, [inet | Opts], Timeout);
+        false ->
+          {error, nxdomain}
+      end
+  end;
+do_connect(Hostname, Port, Opts, Timeout) ->
+  ?report_debug("happy eyeballs, try to connect using IPv6",  [{hostname, Hostname}, {port, Port}]),
+  Self = self(),
+  {Ipv6Addrs, IPv4Addrs} = getaddrs(Hostname),
+  {Pid6, MRef6} = spawn_monitor(fun() -> try_connect(Ipv6Addrs, Port, Opts, Self) end),
+  TRef = erlang:start_timer(?TIMEOUT, self(), try_ipv4),
+  receive
+    {'DOWN', MRef6, _Type, _Pid, {happy_connect, OK}} ->
+      _ = erlang:cancel_timer(TRef, []),
+      OK;
+    {'DOWN', MRef6, _Type, _Pid, _Info} ->
+      _ = erlang:cancel_timer(TRef, []),
+      {Pid4, MRef4} = spawn_monitor(fun() -> try_connect(IPv4Addrs, Port, Opts, Self) end),
+      do_connect_2(Pid4, MRef4, Timeout);
+    {timeout, TRef, try_ipv4} ->
+      PidRef4 = spawn_monitor(fun() -> try_connect(IPv4Addrs, Port, Opts, Self) end),
+      do_connect_1(PidRef4, {Pid6, MRef6}, Timeout)
+  after Timeout -> 
+          _ = erlang:cancel_timer(TRef, []),
+          erlang:demonitor(MRef6, [flush]),
+          {error, connect_timeout}
+  end.
+
+
+do_connect_1({Pid4, MRef4}, {Pid6, MRef6}, Timeout) ->
+  receive
+    {'DOWN', MRef4, _Type, _Pid, {happy_connect, OK}} ->
+      ?report_trace("happy_connect ~p", [OK]),
+      connect_gc(Pid6, MRef6),
+      OK;
+    {'DOWN', MRef4, _Type, _Pid, _Info} ->
+      do_connect_2(Pid6, MRef6, Timeout);
+    {'DOWN', MRef6, _Type, _Pid, {happy_connect, OK}} ->
+      ?report_trace("happy_connect ~p", [OK]),
+      connect_gc(Pid4, MRef4),
+      OK;
+    {'DOWN', MRef6, _Type, Pid, _Info} ->
+      do_connect_2(Pid4, MRef4, Timeout)
+  after Timeout ->
+          connect_gc(Pid4, MRef4),
+          connect_gc(Pid6, MRef6),
+          {error, connect_timeout}
+  end.
+
+do_connect_2(Pid, MRef, Timeout) ->
+  receive
+    {'DOWN', MRef, _Type, _Pid, {happy_connect, OK}} ->
+      ?report_trace("happy_connect ~p", [OK]),
+      OK;
+    {'DOWN', MRef, _Type, _Pid, Info} ->
+      {connect_error, Info}
+  after Timeout ->
+          connect_gc(Pid, MRef),
+          {error, connect_timeout}
+  end.
+
+connect_gc(Pid, MRef) ->
+  catch exit(Pid, normal),
+  erlang:demonitor(MRef, [flush]).
+
+
+-spec parse_address(inet:ip_address() | binary() | string()) -> inet:ip_address() | string().
+parse_address(IPTuple) when is_tuple(IPTuple) -> IPTuple;
+parse_address(IPBin) when is_binary(IPBin) ->
+  parse_address(binary_to_list(IPBin));
+%% IPv6 string with brackets
+parse_address("[" ++ IPString) ->
+  parse_address(lists:sublist(IPString, length(IPString) - 1));
+parse_address(IPString) ->
+  case inet:parse_address(IPString) of
+    {ok, IP} -> IP;
+    {error, _} -> IPString
+  end.
+
+-spec getaddrs(string()) -> {[{inet:ip_address(), 'inet6' | 'inet'}], [{inet:ip_address(), 'inet6' | 'inet'}]}.
+getaddrs("localhost") ->
+  {[{{0,0,0,0,0,0,0,1}, 'inet6'}], [{{127,0,0,1}, 'inet'}]};
+getaddrs(Name) ->
+  IP6Addrs = [{Addr, 'inet6'} || Addr <- getbyname(Name, 'aaaa')],
+  IP4Addrs = [{Addr, 'inet'} || Addr <- getbyname(Name, 'a')],
+  {IP6Addrs, IP4Addrs}.
+
+getbyname(Hostname, Type) ->
+  case (catch inet_res:getbyname(Hostname, Type)) of
+    {'ok', #hostent{h_addr_list=AddrList}} -> lists:usort(AddrList);
+    {error, _Reason} -> [];
+    Else ->
+      %% ERLANG 22 has an issue when g matching somee DNS server messages
+      ?report_debug("DNS error", [{hostname, Hostname}
+                                 ,{type, Type}
+                                 ,{error, Else}]),
+      []
+  end.
+
+try_connect(Ipv6Addrs, Port, Opts, Self) ->
+  try_connect(Ipv6Addrs, Port, Opts, Self, {error, nxdomain}).
+
+try_connect([], _Port, _Opts, _ServerPid, LastError) ->
+  ?report_trace("happy eyeball: failed to connect", [{error, LastError}]),
+  exit(LastError);
+try_connect([{IP, Type} | Rest], Port, Opts, ServerPid, _LastError) ->
+  ?report_trace("try to connect", [{ip, IP}, {type, Type}]),
+  case gen_tcp:connect(IP, Port, [Type | Opts], ?TIMEOUT) of
+    {ok, Socket} = OK ->
+      ?report_trace("success to connect", [{ip, IP}, {type, Type}]),
+      ok = gen_tcp:controlling_process(Socket, ServerPid),
+      exit({happy_connect, OK});
+    Error ->
+      try_connect(Rest, Port, Opts, ServerPid, Error)
+  end.

src/hackney_http_connect.erl
@@ -63,7 +63,7 @@ connect(ProxyHost, ProxyPort, Opts, Timeout)
   ConnectOpts = hackney_util:filter_options(Opts, AcceptedOpts, BaseOpts),
 
   %% connect to the proxy, and upgrade the socket if needed.
-  case gen_tcp:connect(ProxyHost, ProxyPort, ConnectOpts) of
+  case hackney_happy:connect(ProxyHost, ProxyPort, ConnectOpts) of
     {ok, Socket} ->
       case do_handshake(Socket, Host, Port, Opts) of
         ok ->

src/hackney_pool.erl
@@ -63,12 +63,13 @@ checkout(Host, Port, Transport, Client) ->
   Requester = self(),
   try
     do_checkout(Requester, Host, Port, Transport, Client)
-  catch _:_ ->
+  catch _:Error ->
+    ?report_trace("pool: checkout failure", [{error, Error}]),
     {error, checkout_failure}
   end.
 
 do_checkout(Requester, Host, _Port, Transport, #client{options=Opts,
-  mod_metrics=Metrics}=Client) ->
+                                                       mod_metrics=Metrics}=Client) ->
   ConnectTimeout = proplists:get_value(connect_timeout, Opts, 8000),
   %% Fall back to using connect_timeout if checkout_timeout is not set
   CheckoutTimeout = proplists:get_value(checkout_timeout, Opts, ConnectTimeout),
@@ -78,7 +79,6 @@ do_checkout(Requester, Host, _Port, Transport, #client{options=Opts,
   Pool = find_pool(PoolName, Opts),
   case catch gen_server:call(Pool, {checkout, Connection, Requester, RequestRef}, CheckoutTimeout) of
     {ok, Socket, Owner} ->
-
       %% stats
       ?report_debug("reuse a connection", [{pool, PoolName}]),
       _ = metrics:update_meter(Metrics, [hackney_pool, PoolName, take_rate], 1),
@@ -105,7 +105,7 @@ do_checkout(Requester, Host, _Port, Transport, #client{options=Opts,
           _ = metrics:increment_counter(Metrics, [hackney, Host, connect_timeout]),
           {error, timeout};
         Error ->
-          ?report_trace("connect error", []),
+          ?report_trace("connect error", [{pool, PoolName}, {error, Error}]),
           _ = metrics:increment_counter(Metrics, [hackney, Host, connect_error]),
           Error
       end;

src/hackney_socks5.erl
@@ -62,7 +62,7 @@ connect(Host, Port, Opts, Timeout) when is_list(Host), is_integer(Port),
   ConnectOpts = hackney_util:filter_options(Opts, AcceptedOpts, BaseOpts),
 
   %% connect to the socks 5 proxy
-  case gen_tcp:connect(ProxyHost, ProxyPort, ConnectOpts, Timeout) of
+  case hackney_happy:connect(ProxyHost, ProxyPort, ConnectOpts, Timeout) of
     {ok, Socket} ->
       case do_handshake(Socket, Host, Port, Opts) of
         ok ->

commit a10c4f4ed94c301924a34d3f3fc698042214c610

fix SSL timeout guard clause typo causing checkout_failure
Fixes typo introduced in c232dee where infinity was accidentally
changed to 5000 in the timeout guard, causing SSL connections
with other timeout values to fail.

Resolves checkout_failure errors reported in #763.

src/hackney_ssl.erl
@@ -126,7 +126,7 @@ connect(Host, Port, Opts) ->
   connect(Host, Port, Opts, 30000).
 
 connect(Host, Port, Opts0, Timeout) when is_list(Host), is_integer(Port),
-                                        (Timeout =:= 5000 orelse is_integer(Timeout)) ->
+                                        (Timeout =:= infinity orelse is_integer(Timeout)) ->
   SSLOpts = proplists:get_value(ssl_options, Opts0),
   BaseOpts = [binary, {active, false}, {packet, raw}],
   Opts1 = hackney_util:merge_opts(BaseOpts, proplists:delete(ssl_options, Opts0)),