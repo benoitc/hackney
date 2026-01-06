6edcb13c9de22024018b1d8eca2111bdadbaad6f
Benoit Chesneau <bchesneau@gmail.com>
2026-01-06 16:12:14 +0100

    docs: update HTTP/2 demo to show protocols option

--- examples/http2_demo.erl
+++ examples/http2_demo.erl
@@ -8,18 +8,21 @@
 %%   2> http2_demo:run().
 
 run() ->
-    io:format("~n"),
-    io:format("=== Hackney HTTP/2 Demo ===~n~n"),
+    io:format("~n=== Hackney HTTP/2 Demo ===~n~n"),
 
-    {ok, Status, Headers, Body} = hackney:get(
-        <<"https://nghttp2.org/">>,
-        [],
-        <<>>,
-        [with_body]
-    ),
+    %% HTTP/2 request (default)
+    io:format("HTTP/2 request:~n"),
+    {ok, S1, H1, _} = hackney:get(<<"https://nghttp2.org/">>, [], <<>>, [with_body]),
+    io:format("  Status: ~p~n", [S1]),
+    io:format("  Headers: lowercase (HTTP/2)~n"),
+    io:format("  Example: ~p~n~n", [element(1, hd(H1))]),
 
-    io:format("  Status:   ~p~n", [Status]),
-    io:format("  Server:   ~s~n", [proplists:get_value(<<"server">>, Headers, <<>>)]),
-    io:format("  Via:      ~s~n", [proplists:get_value(<<"via">>, Headers, <<>>)]),
-    io:format("  Body:     ~p bytes~n~n", [byte_size(Body)]),
+    %% HTTP/1.1 request (forced via protocols option)
+    io:format("HTTP/1.1 request (protocols=[http1]):~n"),
+    {ok, S2, H2, _} = hackney:get(<<"https://nghttp2.org/">>, [], <<>>,
+                                  [with_body, {protocols, [http1]}]),
+    io:format("  Status: ~p~n", [S2]),
+    io:format("  Headers: mixed case (HTTP/1.1)~n"),
+    io:format("  Example: ~p~n", [element(1, hd(H2))]),
+
     ok.
