159d18fba9d34f735801da5ed284231868e2db5d
Benoit Chesneau <bchesneau@gmail.com>
2026-01-27 22:13:59 +0100

    feat(http3): add sockname/1 support for HTTP/3 connections
    Add HTTP/3 handler for sockname operation in hackney_conn.erl.
    When h3_conn is defined, delegates to hackney_quic:sockname/1 to
    retrieve the local socket address for QUIC connections.

--- src/hackney_conn.erl
+++ src/hackney_conn.erl
@@ -1456,6 +1456,11 @@ handle_common({call, From}, peername, _State,
     Result = hackney_quic:peername(H3Conn),
     {keep_state_and_data, [{reply, From, Result}]};
 
+handle_common({call, From}, sockname, _State,
+              #conn_data{h3_conn = H3Conn} = _Data) when H3Conn =/= undefined ->
+    Result = hackney_quic:sockname(H3Conn),
+    {keep_state_and_data, [{reply, From, Result}]};
+
 %% Low-level send operation
 handle_common({call, From}, {send, Data}, _State, #conn_data{transport = Transport, socket = Socket} = _Data)
   when Socket =/= undefined ->
